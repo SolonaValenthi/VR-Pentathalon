b5814996298d337bda09578e311de5a3f7e3f2c7
Nikolai Nelson <nikolainelson@comcast.net>
2024-03-23 06:56:51 -0700

    key turns when put into lock, door opens.

--- Assets/Scripts/KeyholeBehavior.cs
+++ Assets/Scripts/KeyholeBehavior.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class KeyholeBehavior : MonoBehaviour
 {
+    [SerializeField] private GameObject _keyAttach;
     [SerializeField] private GameObject _key;
+    [SerializeField] private GameObject _usedKey;
+
+    GameObject _fakeKey;
 
     public void TurnKey()
     {
-        Debug.Log("keyhole selcted.");
         StartCoroutine(Key());
     }
 
     IEnumerator Key()
     {
         float elapsed = 0.0f;
+        yield return new WaitForSeconds(0.1f);
+        _fakeKey = Instantiate(_usedKey, _key.transform.position, _key.transform.rotation);
+        _key.SetActive(false);
 
         while (elapsed < 1)
         {
-            _key.transform.Rotate(Vector3.back * 90 * Time.deltaTime);
+            _fakeKey.transform.Rotate(Vector3.back * 90 * Time.deltaTime);
             elapsed += Time.deltaTime;
             yield return null;
         }
